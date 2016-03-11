9c99c8badbd072901bf70ac1b5e009e8b4852c9e
Luiza Pagliari <lpagliari@gmail.com>
2016-03-11 17:03:39 -0300

    Dynamically set margin-top when zoom is not 100%

--- static/js/fixSmallZooms.js
+++ static/js/fixSmallZooms.js
@@ -7,23 +7,33 @@
 var _ = require('ep_etherpad-lite/static/js/underscore');
 var utils = require("./utils");
 
+// this was calculated using 100% zoom on Chrome
+var DEFAULT_CHAR_WIDTH = 7.2;
+var DEFAULT_LINE_HEIGHT = 16.0;
+
 var ELEMENTS_WITH_MARGINS = [
+  "heading",
+  "action",
   "character",
   "parenthetical",
   "dialogue",
   "transition",
+  "shot",
 ];
 
 var DEFAULT_MARGINS = {
   // these values were originally set on CSS
-  "character":     { left: 152, right: 16 },
-  "parenthetical": { left: 105, right: 154 },
-  "dialogue":      { left: 77,  right: 111 },
-  "transition":    { left: 291, right: 36 },
+  "heading":       { vertical: { top: 2*DEFAULT_LINE_HEIGHT } },
+  "shot":          { vertical: { top: 2*DEFAULT_LINE_HEIGHT } },
+  "action":        { vertical: { top: 1*DEFAULT_LINE_HEIGHT } },
+  "character":     { vertical: { top: 1*DEFAULT_LINE_HEIGHT },
+                     horizontal: { left: 152, right: 16  } },
+  "transition":    { vertical: { top: 1*DEFAULT_LINE_HEIGHT },
+                     horizontal: { left: 291, right: 36  } },
+  "parenthetical": { horizontal: { left: 105, right: 154 } },
+  "dialogue":      { horizontal: { left: 77,  right: 111 } },
 };
 exports.DEFAULT_MARGINS = DEFAULT_MARGINS;
-
-var DEFAULT_CHAR_WIDTH = 7.2; // this was calculated using 100% zoom on Chrome
 
 exports.init = function() {
   waitForResizeToFinishThenCall(function() {
@@ -44,10 +54,11 @@ var waitForResizeToFinishThenCall = function(callback) {
 
 var updateMargins = function() {
   // Use char proportion to find how to adjust margins
-  var newCharProportion = calculateMarginProportion();
+  var newVerticalProportion = calculateVerticalProportion();
+  var newHorizontalProportion = calculateHorizontalProportion();
 
   var elementStyles = _.map(ELEMENTS_WITH_MARGINS, function(elementName) {
-    return getNewStyleForElement(elementName, newCharProportion);
+    return getNewStyleForElement(elementName, newHorizontalProportion, newVerticalProportion);
   }).join("\n");
 
   // we don't want to affect mobile screens
@@ -57,26 +68,48 @@ var updateMargins = function() {
   utils.getPadInner().find("head").append("<style>" + style + "</style>");
 }
 
-var calculateMarginProportion = function() {
+var calculateVerticalProportion = function() {
+  var oneLineHeight = getHeightOfOneLine();
+  var charProportion = oneLineHeight / DEFAULT_LINE_HEIGHT;
+
+  return charProportion;
+}
+
+var calculateHorizontalProportion = function() {
   var oneCharWidth = getWidthOfOneChar();
   var charProportion = oneCharWidth / DEFAULT_CHAR_WIDTH;
 
   return charProportion;
 }
 
-var getNewStyleForElement = function(elementName, charProportion) {
+var getNewStyleForElement = function(elementName, horizontalProportion, verticalProportion) {
   var defaultMargins = DEFAULT_MARGINS[elementName];
-  var marginLeft = charProportion * defaultMargins.left;
-  var marginRight = charProportion * defaultMargins.right;
 
-  // this was moved from CSS file to here, so it can have a dynamic value
-  var marginLeftStyle = "margin-left: " + marginLeft + "px;";
-  var marginRightStyle = "margin-right: " + marginRight + "px;";
-  var elementStyle = elementName + " { " + marginLeftStyle + marginRightStyle + " }";
+  var marginLeft  = getMarginStyle("left" , defaultMargins.horizontal, horizontalProportion);
+  var marginRight = getMarginStyle("right", defaultMargins.horizontal, horizontalProportion);
+  var marginTop   = getMarginStyle("top"  , defaultMargins.vertical  , verticalProportion);
+
+  var elementStyle = elementName + " { " + marginLeft + marginRight + marginTop + " }";
 
   return elementStyle;
 }
 
+var getMarginStyle = function(marginName, defaultValues, proportion) {
+  // if there's no default value, return an empty string
+  var marginStyle = "";
+
+  if (defaultValues && defaultValues[marginName]) {
+    var newMargingValue = proportion * defaultValues[marginName];
+    marginStyle = "margin-" + marginName + ": " + newMargingValue + "px; ";
+  }
+
+  return marginStyle;
+}
+
 var getWidthOfOneChar = function() {
   return utils.getPadOuter().find("#linemetricsdiv").get(0).getBoundingClientRect().width;
+}
+
+var getHeightOfOneLine = function() {
+  return utils.getPadOuter().find("#linemetricsdiv").get(0).getBoundingClientRect().height;
 }
