0251e9ccc39f10926dea809b9930311ea9fa7002
joassouza <joassouzasantos@gmail.com>
2016-09-30 11:55:58 -0300

    Implement rules of paste of SM on SE
    This commit fixes the problem of broken SM when user pastes a
    partial SM. Now when a partial SM is pasted the lines are changed
    to actions.

--- static/js/pasteOnSE.js
+++ static/js/pasteOnSE.js
@@ -4,6 +4,8 @@ var utils   = require('./utils');
 var SMUtils = require('ep_script_scene_marks/static/js/utils');
 
 var PASTE_ON_SE_CLASS = '.pasteOnSE';
+var PART_OF_COMPLETE_SM_CLASS = 'part_of_sm_complete';
+var TAG_ALREADY_PROCESSED_CLASS = 'processed_find_sm_interval';
 
 // This hook only collect text nodes. Empty nodes, e.g. <tag><br></tag>, are not collected in this hook
 exports.collectContentLineText = function(hook, context) {
@@ -11,7 +13,70 @@ exports.collectContentLineText = function(hook, context) {
   var pasteOnSE = $lineTargetOfPaste.length;
   if(pasteOnSE){
     removeEmptyHiddenSceneMarks($lineTargetOfPaste);
+    changeToActionIfItIsPartOfAnInvalidSM(context, $lineTargetOfPaste);
   }
+}
+
+// we change to actions every scene mark which is not part of a valid scene mark
+// (e.g. when user pastes a act_summary and heading)
+var changeToActionIfItIsPartOfAnInvalidSM = function(context, $lineTargetOfPaste){
+  var $node = $(context.node);
+  var state = context.state;
+  var lineAttributes = state.lineAttributes;
+  var $linesPasted = $lineTargetOfPaste.children("div");
+  var hasSceneMarkPasted = $linesPasted.find('act_name, act_summary, sequence_name, sequence_summary').length;
+
+  if(!hasSceneMarkPasted){
+    return;
+  }
+  markSceneMarksValid($linesPasted);
+  changeInvalidSceneMarksToAction($node, lineAttributes);
+}
+
+var markSceneMarksValid = function($linesPasted) {
+  _.each($linesPasted, function(line){
+    var $line = $(line);
+    var tagAlreadyProcessed = $line.filter('.' + TAG_ALREADY_PROCESSED_CLASS).length;
+    var isSceneMark = $line.find('act_name, act_summary, sequence_name, sequence_summary').length;
+
+    if(!tagAlreadyProcessed && isSceneMark){
+      // take all scene marks until the heading
+      var $lines = $line.nextUntil('div:has(heading)').addBack();
+      var linesHaveHeadingOfReference = $lines.last().next().find('heading').length;
+
+      // avoid reprocessing this scene marks range in the next iteration
+      $lines.addClass(TAG_ALREADY_PROCESSED_CLASS);
+      var isSceneMarkTitle = $line.find('act_name, sequence_name').length;
+
+      if(isSceneMarkTitle && linesHaveHeadingOfReference){
+        // mark scene marks range as complete scene mark, so we keep them
+        // when the Etherpad collects the line
+        $lines.addClass(PART_OF_COMPLETE_SM_CLASS);
+      }
+    }
+  });
+}
+
+var changeInvalidSceneMarksToAction = function($node, lineAttributes) {
+  var isASceneMark = lineAttributes.act_scene_mark || lineAttributes.sequence_scene_mark;
+  var isNodePartOfAValidSceneMark = $node.closest('div').hasClass(PART_OF_COMPLETE_SM_CLASS);
+  if(isASceneMark && !isNodePartOfAValidSceneMark){
+    resetPreviousLineAttribs(lineAttributes);
+    transformLineToAction(lineAttributes);
+  }
+}
+
+// delete all attrib if it is not default attribs
+var resetPreviousLineAttribs = function(lineAttributes){
+  var lineAttributesKeys = _.keys(lineAttributes);
+  var lineAttributesKeysWithoutDefaultLineAttribs = _.difference(lineAttributesKeys, utils.DEFAULT_LINE_ATTRIBS);
+  _.each(lineAttributesKeysWithoutDefaultLineAttribs, function(keysToRemove){
+    delete(lineAttributes[keysToRemove]);
+  });
+}
+
+var transformLineToAction = function(lineAttributes) {
+  lineAttributes['script_element'] = 'action';
 }
 
 var removeEmptyHiddenSceneMarks = function($line) {
--- static/tests/frontend/specs/paste.js
+++ static/tests/frontend/specs/paste.js
@@ -1,6 +1,7 @@
 describe('ep_script_elements - handle paste on script elements', function () {
   var utils, helperFunctions;
   var SHOT_LINE = 6;
+  var GENERAL_LINE = 9;
 
   before(function (done) {
     utils = ep_script_elements_test_helper.utils;
@@ -70,21 +71,110 @@ describe('ep_script_elements - handle paste on script elements', function () {
     });
   });
 
-  // TODO
   context('when user copies part of a scene mark', function(){
+    before(function (done) {
+      var _this = this;
+      utils.placeCaretInTheBeginningOfLine(GENERAL_LINE, function(){
+        helperFunctions.simulatePasteOfPartialSceneMark(GENERAL_LINE, function(){
+          helperFunctions.waitCollectLinesPasted(_this, GENERAL_LINE, done);
+        });
+      });
+    });
+    it('pastes the scene marks as actions', function(done){
+      utils.validateLineTextAndType(GENERAL_LINE, 'asummary', 'action');
+      utils.validateLineTextAndType(GENERAL_LINE + 1, 'sname', 'action');
+      done();
+    });
 
+    context('when user performs undo', function(){
+      before(function () {
+        utils.undo();
+      });
+
+      it('returns to the original text', function(done){
+        helperFunctions.hasTheOriginalText();
+        done();
+      });
+    });
   });
 
-  // TODO
   // user can copy part of an act, e.g. from sequence_name until the heading,
   // this case we can consider as a full scene mark
   context('when user copies part of a scene mark and its heading', function(){
+    before(function (done) {
+      var _this = this;
+      utils.placeCaretInTheBeginningOfLine(GENERAL_LINE, function(){
+        helperFunctions.simulatePasteOfPartialSceneMarkAndHeading(GENERAL_LINE, function(){
+          helperFunctions.waitCollectLinesPasted(_this, GENERAL_LINE, done);
+        });
+      });
+    });
+    it('pastes the scene marks as actions and keeps the heading', function(done){
+      utils.validateLineTextAndType(GENERAL_LINE, 'asummary', 'action');
+      utils.validateLineTextAndType(GENERAL_LINE + 1, 'sname', 'action');
+      utils.validateLineTextAndType(GENERAL_LINE + 2, 'ssummary', 'action');
+      utils.validateLineTextAndType(GENERAL_LINE + 3, 'heading', 'heading');
+      done();
+    });
 
+    context('when user performs undo', function(){
+      before(function () {
+        utils.undo();
+      });
+
+      it('returns to the original text', function(done){
+        helperFunctions.hasTheOriginalText();
+        done();
+      });
+    });
   });
 
-  // TODO
-  context('when user copies all the scene mark and its heading', function(){
+  context('when user copies all the scene mark without a heading', function(){
+    before(function (done) {
+      var _this = this;
+      utils.placeCaretInTheBeginningOfLine(GENERAL_LINE, function(){
+        helperFunctions.simulatePasteFullSceneMarkWithoutAHeading(GENERAL_LINE, function(){
+          helperFunctions.waitCollectLinesPasted(_this, GENERAL_LINE, done);
+        });
+      });
+    });
+    it('pastes the scene marks as actions', function(done){
+      utils.validateLineTextAndType(GENERAL_LINE, 'aname', 'action');
+      utils.validateLineTextAndType(GENERAL_LINE + 1, 'asummary', 'action');
+      utils.validateLineTextAndType(GENERAL_LINE + 2, 'sname', 'action');
+      utils.validateLineTextAndType(GENERAL_LINE + 3, 'ssummary', 'action');
+      done();
+    });
 
+    context('when user performs undo', function(){
+      before(function () {
+        utils.undo();
+      });
+
+      it('returns to the original text', function(done){
+        helperFunctions.hasTheOriginalText();
+        done();
+      });
+    });
+  });
+
+  context('when user copies all the scene mark and its heading', function(){
+    before(function (done) {
+      var _this = this;
+      utils.placeCaretInTheBeginningOfLine(GENERAL_LINE, function(){
+        helperFunctions.simulatePasteFullSceneMarkWithAHeading(GENERAL_LINE, function(){
+          helperFunctions.waitCollectLinesPasted(_this, GENERAL_LINE, done);
+        });
+      });
+    });
+    it('pastes the scene marks and the heading', function(done){
+      utils.validateLineTextAndType(GENERAL_LINE, 'aname', 'act_name');
+      utils.validateLineTextAndType(GENERAL_LINE + 1, 'asummary', 'act_summary');
+      utils.validateLineTextAndType(GENERAL_LINE + 2, 'sname', 'sequence_name');
+      utils.validateLineTextAndType(GENERAL_LINE + 3, 'ssummary', 'sequence_summary');
+      utils.validateLineTextAndType(GENERAL_LINE + 4, 'heading', 'heading');
+      done();
+    });
   });
 });
 
@@ -101,6 +191,7 @@ ep_script_elements_test_helper.pasteOnSE = {
     var dialogueText  = "------";
     var shotText      = ' ';
     var actionText    = "action";
+    var generalText   = "general";
     var lastLineText  = lastHeading;
 
     var act            = SMUtils.act(smText);
@@ -109,12 +200,13 @@ ep_script_elements_test_helper.pasteOnSE = {
     var dialogue       = utils.dialogue(dialogueText);
     var shot           = utils.shot(shotText);
     var action         = utils.action(actionText);
+    var general        = utils.general(generalText)
     var secondAct      = SMUtils.act(secondSMText);
     var secondSequence = SMUtils.sequence(secondSMText);
     var secondHeading  = utils.heading(lastHeading);
 
     var script = act + sequence + heading +  dialogue + shot + dialogue +
-     action + secondAct + secondSequence + secondHeading;
+     action + general + secondAct + secondSequence + secondHeading;
 
     utils.createScriptWith(script, lastLineText, done)
 
@@ -127,7 +219,7 @@ ep_script_elements_test_helper.pasteOnSE = {
       '<div><sm_icon><empty /></sm_icon><trash_icon><empty /></trash_icon><sequence_name><span /></sequence_name></div>' +
       '<div><sequence_summary><span /></sequence_summary></div>' +
       '<div><heading /></div>';
-    this.pasteText(copiedHTML, lineTargetOfPaste, cb);
+    this.pasteTextOnChildren(copiedHTML, lineTargetOfPaste, cb);
   },
   simulatePasteOfASelectionIncludingASMHidden: function(lineTargetOfPaste, cb) {
     var copiedHTML =
@@ -138,12 +230,79 @@ ep_script_elements_test_helper.pasteOnSE = {
       '<div><sequence_summary><span /></sequence_summary></div>' +
       '<div><heading><span>HEADING</span></heading></div>' +
       '<div><action><span>action 2</span></action></div>';
-    this.pasteText(copiedHTML, lineTargetOfPaste, cb);
+    this.pasteTextOnChildren(copiedHTML, lineTargetOfPaste, cb);
+  },
+  simulatePasteOfPartialSceneMark: function(lineTargetOfPaste, cb) {
+    var copiedHTML =
+      '<div><act_summary><span>asummary</span></act_summary></div>' +
+      '<div>' +
+        '<sm_icon><empty></empty></sm_icon>' +
+        '<trash_icon><empty></empty></trash_icon>' +
+        '<sequence_name><span>sname</span></sequence_name>' +
+      '</div>';
+    this.pasteTextOnLine(copiedHTML, lineTargetOfPaste, cb)
+  },
+  simulatePasteOfPartialSceneMarkAndHeading: function(lineTargetOfPaste, cb) {
+    var copiedHTML =
+      '<div><act_summary><span>asummary</span></act_summary></div>' +
+      '<div>' +
+        '<sm_icon><empty></empty></sm_icon>' +
+        '<trash_icon><empty></empty></trash_icon>' +
+        '<sequence_name><span>sname</span></sequence_name>' +
+      '</div>' +
+      '<div><act_summary><span>ssummary</span></act_summary></div>' +
+      '<div><heading><span>heading</span></heading></div>';
+    this.pasteTextOnLine(copiedHTML, lineTargetOfPaste, cb)
+  },
+  simulatePasteFullSceneMarkWithoutAHeading: function(lineTargetOfPaste, cb) {
+    var copiedHTML =
+      '<div>' +
+        '<sm_icon><empty></empty></sm_icon>' +
+        '<trash_icon><empty></empty></trash_icon>' +
+        '<act_name><span>aname</span></act_name>' +
+      '</div>' +
+      '<div><act_summary><span>asummary</span></act_summary></div>' +
+      '<div>' +
+        '<sm_icon><empty></empty></sm_icon>' +
+        '<trash_icon><empty></empty></trash_icon>' +
+        '<sequence_name><span>sname</span></sequence_name>' +
+      '</div>' +
+      '<div><act_summary><span>ssummary</span></act_summary></div>';
+    this.pasteTextOnLine(copiedHTML, lineTargetOfPaste, cb)
+  },
+  simulatePasteFullSceneMarkWithAHeading: function(lineTargetOfPaste, cb) {
+    var copiedHTML =
+      '<div>' +
+        '<sm_icon><empty></empty></sm_icon>' +
+        '<trash_icon><empty></empty></trash_icon>' +
+        '<act_name><span>aname</span></act_name>' +
+      '</div>' +
+      '<div><act_summary><span>asummary</span></act_summary></div>' +
+      '<div>' +
+        '<sm_icon><empty></empty></sm_icon>' +
+        '<trash_icon><empty></empty></trash_icon>' +
+        '<sequence_name><span>sname</span></sequence_name>' +
+      '</div>' +
+      '<div><sequence_summary><span>ssummary</span></sequence_summary></div>' +
+      '<div><heading><span>heading</span></heading></div>';
+    this.pasteTextOnLine(copiedHTML, lineTargetOfPaste, cb)
   },
   getLineTarget: function(line) {
     return helper.padInner$("div").slice(line, line + 1);
   },
-  pasteText: function(htmlToPaste, lineTargetOfPaste, cb) {
+  pasteTextOnLine: function(htmlToPaste, lineTargetOfPaste, cb) {
+    var _this = this;
+    setTimeout(function() {
+      _this.triggerEventPaste();
+
+      // WARNING: here we assume the element has only one children, with headings won't work
+      var $lineTarget = _this.getLineTarget(lineTargetOfPaste);
+      $lineTarget.html(htmlToPaste);
+      debugger;
+      cb();
+    }, 1000);
+  },
+  pasteTextOnChildren: function(htmlToPaste, lineTargetOfPaste, cb) {
     var _this = this;
     setTimeout(function() {
       _this.triggerEventPaste();
@@ -182,10 +341,11 @@ ep_script_elements_test_helper.pasteOnSE = {
     utils.validateLineTextAndType(6, ' ', 'shot');
     utils.validateLineTextAndType(7, '------', 'dialogue');
     utils.validateLineTextAndType(8, 'action', 'action');
-    utils.validateLineTextAndType(9, 'ACT OF SM 2 TEXT', 'act_name');
-    utils.validateLineTextAndType(10, 'SUMMARY OF ACT OF SM 2 TEXT', 'act_summary');
-    utils.validateLineTextAndType(11, 'SEQUENCE OF SM 2 TEXT', 'sequence_name');
-    utils.validateLineTextAndType(12, 'SUMMARY OF SEQUENCE OF SM 2 TEXT', 'sequence_summary');
-    utils.validateLineTextAndType(13, 'HEADING 2', 'heading');
+    utils.validateLineTextAndType(9, 'general', 'general');
+    utils.validateLineTextAndType(10, 'ACT OF SM 2 TEXT', 'act_name');
+    utils.validateLineTextAndType(11, 'SUMMARY OF ACT OF SM 2 TEXT', 'act_summary');
+    utils.validateLineTextAndType(12, 'SEQUENCE OF SM 2 TEXT', 'sequence_name');
+    utils.validateLineTextAndType(13, 'SUMMARY OF SEQUENCE OF SM 2 TEXT', 'sequence_summary');
+    utils.validateLineTextAndType(14, 'HEADING 2', 'heading');
   },
 }
